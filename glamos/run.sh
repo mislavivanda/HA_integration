b90072ab624410ea7ce5c24ddebc1cc3ffb3b8de
mislavivanda <mislavivanda454@gmail.com>
2023-12-20 14:42:30 +0100

    3D view integrated with HA API version 1 build

--- glamos/run.sh
+++ glamos/run.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 set -e
+
+# lshw -class cpu
+# lshw -class processor
+# cat /proc/cpuinfo
+# echo "Command 2"
+# echo "Command 3"
+# lshw | grep "uuid"
+# lshw | grep "serial"
+# # echo "Command 4"
+# cat /proc/cpuinfo | grep "Serial"
+# # echo "Command1"
+# cpuid | grep "processor serial number:"
+
+# cat /sys/class/dmi/id/product_uuid
+# ls
+# ls /var/lib
+# ls /etc
+# cat /var/lib/dbus/machine-id
+# hostnamectl
+
 CONFIG_PATH=/data/options.json
 
 HA_PORT=$(jq --raw-output '.ha_port // empty' $CONFIG_PATH)
